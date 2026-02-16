95c4c92259e757b5005ff52d0cd02bd607b652e5
karwa <neeraj>
2026-02-16 11:23:48 +0530

    modified movefiles.sh

--- scripts/move_files.sh
+++ scripts/move_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-mkdir -p /home/ubuntu/deploy-temp
+echo "Removing default nginx files"
 rm -rf /usr/share/nginx/html/*
+
+echo "Copying deployed files"
 cp -r /home/ubuntu/deploy-temp/* /usr/share/nginx/html/
+
+echo "Restarting nginx"
+systemctl restart nginx
